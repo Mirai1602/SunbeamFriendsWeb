3efd0b9feede5e39487d12c05297ab4fcc2aff89
camijavi <camijavinavarro@gmail.com>
2026-08-29 14:23:09 -0600

    implement client-side registration form validation and integrate Firebase Auth services

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -1,8 +1,9 @@
 // firebase api keys and initialization
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
